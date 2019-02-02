ca637680e9c0e689d7deef9c8c3d3b73979e5397
Andrey Serikov <grtrash2@gmail.com>
2019-02-02 16:42:40 +0300

    Add login form

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-class Config():
-    SECRET_KEY = os.environ.get('SEKRET_KEY') or 'you-will-never-guess-it'
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-it'
