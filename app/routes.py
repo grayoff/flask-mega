404e7a793db00bd7cdec2b964f87c1cb53b8335c
Andrey Serikov <grtrash2@gmail.com>
2019-02-02 15:01:28 +0300

    Add first blog posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Grayoff'}
-    return render_template('index.html', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day today!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Movie was so cool!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
